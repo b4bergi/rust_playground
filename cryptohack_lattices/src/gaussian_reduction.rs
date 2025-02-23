9ee6a97e90ef15d8b9aed5e88f366d1ba6ef9b0f
b4bergi <=>
2025-02-23 18:27:05 +0100

    solved "find the lattice"

--- cryptohack_lattices/src/gaussian_reduction.rs
+++ cryptohack_lattices/src/gaussian_reduction.rs
@@ -1,17 +1,19 @@
 extern crate nalgebra as na;
 
 use nalgebra::Vector2;
-pub fn reduce(mut v1: Vector2<i128>, mut v2: Vector2<i128>) -> (Vector2<i128>, Vector2<i128>) {
+use num_bigint::BigInt;
+
+pub fn reduce(mut v1: Vector2<BigInt>, mut v2: Vector2<BigInt>) -> (Vector2<BigInt>, Vector2<BigInt>) {
     loop {
         if v1.dot(&v1) > v2.dot(&v2) {
             std::mem::swap(&mut v1, &mut v2);
         }
 
         let m = v1.dot(&v2) / v1.dot(&v1);
-        if m == 0 {
+        if m == BigInt::from(0) {
             return (v1, v2);
         }
-        let scaled_v1 = v1.map(|x| x*m);
+        let scaled_v1 = v1.map(|x| x*&m);
         v2 = v2 - scaled_v1;
     }
 }
